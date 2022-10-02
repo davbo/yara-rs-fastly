b5ac6395ae8a87c639632c24c77217448517bda3
Dave King <dave@davbo.uk>
2022-10-02 17:32:56 +0300

    Setup with yara-rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
 //! Compute@Edge static content starter kit program.
-
 mod config;
 
 use fastly::http::{header, HeaderValue, Method, StatusCode};
 use fastly::{Error, Request, Response};
-
-cfg_if::cfg_if! {
-    if #[cfg(feature = "auth")] {
-        mod awsv4;
-        use chrono::Utc;
-        use crate::awsv4::hash;
-        use fastly::handle::dictionary::DictionaryHandle;
-    }
-}
+use yara::yara_match;
 
 /// The entry point for your application.
 ///
@@ -55,16 +46,8 @@ fn main(mut req: Request) -> Result<Response, Error> {
             .with_content_type(fastly::mime::TEXT_PLAIN));
     }
 
-    // Append index.html if path is a directory.
-    if req.get_path().ends_with('/') {
-        req.set_path(&format!("{}index.html", req.get_path()));
-    }
-
     // Remove the query string to improve cache hit ratio.
     req.remove_query();
-
-    // Assign the path to a variable to be used later.
-    let original_path = req.get_path().to_owned();
 
     // Set the `Host` header to the bucket host rather than our C@E endpoint.
     req.set_header(
@@ -73,158 +56,37 @@ fn main(mut req: Request) -> Result<Response, Error> {
     );
 
     // Copy the modified client request to create a backend request.
-    let mut bereq = req.clone_without_body();
-
-    // Authenticate the initial request to the origin.
-    set_authentication_headers(&mut bereq);
-
-    // Set the cache TTL for the expected result of the request.
-    let ttl = get_cache_ttl(bereq.get_path());
-    bereq.set_ttl(ttl);
+    let bereq = req.clone_without_body();
 
     // Send the request to the backend and assign its response to `beresp`.
     let mut beresp = bereq.send(config::BACKEND_NAME)?;
 
-    // If backend response is 404, try for index.html
-    if is_not_found(&beresp) && !original_path.ends_with("index.html") {
-        // Copy the original request and append index.html to the path.
-        bereq = req.clone_without_body();
-        bereq.set_path(&format!("{}/index.html", original_path));
-
-        // Send the request to the backend.
-        set_authentication_headers(&mut bereq);
-        beresp = bereq.send(config::BACKEND_NAME)?;
-
-        // If file exists, trigger redirect with `/` appended to path.
-        // This means the canonical URL for index pages will always end with a trailing slash.
-        if !is_not_found(&beresp) {
-            beresp = Response::from_status(StatusCode::MOVED_PERMANENTLY)
-                .with_header(header::LOCATION, &format!("{}/", original_path));
-            return Ok(beresp);
-        }
-    }
-
-    // If backend response is still 404, serve the 404.html file from the bucket.
-    if is_not_found(&beresp) {
-        // Copy the original request and replace the path with /404.html.
-        bereq = req.clone_without_body();
-        bereq.set_path("/404.html");
-
-        // Send the request to the backend.
-        set_authentication_headers(&mut bereq);
-        beresp = bereq.send(config::BACKEND_NAME)?;
-    }
-
     filter_headers(&mut beresp);
 
-    // Add Cache-Control header to response with same TTL as used internally.
-    beresp.set_header(header::CACHE_CONTROL, format!("public, max-age={}", ttl));
+    let body = beresp.into_body();
+    let body_bytes = body.into_bytes();
 
-    // The following headers should only be added to HTML responses.
-    if let Some(header) = beresp.get_header(header::CONTENT_TYPE) {
-        if header.to_str().unwrap().starts_with("text/html") {
-            // Apply referrer-policy and HSTS to HTML pages.
-            beresp.set_header(header::REFERRER_POLICY, "origin-when-cross-origin");
-            beresp.set_header(header::STRICT_TRANSPORT_SECURITY, "max-age=2592000");
-
-            // Apply Access-Control-Allow-Origin to allow cross-origin resource sharing.
-            beresp.set_header(header::ACCESS_CONTROL_ALLOW_ORIGIN, allowed_origins);
-
-            // Set Content-Security-Policy header to prevent loading content from other origins.
-            beresp.set_header(
-                header::CONTENT_SECURITY_POLICY,
-                config::CONTENT_SECURITY_POLICY,
-            );
-
-            // Set X-Frame-Options header to prevent other origins embedding the site.
-            beresp.set_header(header::X_FRAME_OPTIONS, "SAMEORIGIN");
-
-            // Add headers to preload assets such as fonts needed across your site:
-            //
-            // For example, preload Roboto Mono on all pages. NOTE: Usually, you would use "font" for the "as" parameter of a link to a font source,
-            // but Google Fonts serves a CSS file that will load the individual font files as needed so we use "style".
-            beresp.append_header(
-                header::LINK,
-                format!(
-                    "<{}>; rel=preload; as={};",
-                    "https://fonts.googleapis.com/css?family=Roboto+Mono", "style"
-                ),
-            );
-            // ... add your own here
-        }
-    }
-
-    // Compress assets.
-    if original_path.starts_with("/assets/") {
-        // C@E services currently run behind h2o (https://github.com/h2o/h2o)
-        // This header will enable compression on the downstream message in h2o
-        beresp.set_header("X-Compress-Hint", "on");
-    }
-
-    // Return the backend response to the client.
-    Ok(beresp)
-}
-
-/// Determines the cache TTL that should be used for an object at a given path.
-/// The paths used here are just examples, you can modify this however you want to cache your objects intelligently.
-fn get_cache_ttl(path: &str) -> u32 {
-    // Assets should be identified with a hash so they can have a long TTL.
-    if path.starts_with("/assets") {
-        return 60 * 60 * 24;
-    }
-
-    // Resource pages need up-to-date data, so cache them for less time.
-    // We check the `/` count in the path to determine whether the request is for the index or an individual resource.
-    if path.starts_with("/resources/") && path.split('/').count() > 3 {
-        return 60;
-    }
-
-    // Any other content can be cached for 5 minutes.
-    return 60 * 5;
-}
-
-/// Determines if a backend response indicates the requested file doesn't exist.
-fn is_not_found(resp: &Response) -> bool {
-    resp.get_status() == StatusCode::NOT_FOUND || resp.get_status() == StatusCode::FORBIDDEN
-}
-
-/// Sets authentication headers for a given request.
-#[cfg(feature = "auth")]
-fn set_authentication_headers(req: &mut Request) {
-    // Ensure that request is a GET to prevent signing write operations
-    if req.get_method() != Method::GET {
-        return;
-    }
-
-    let auth = match DictionaryHandle::open("bucket_auth") {
-        Ok(h) if h.is_valid() => h,
-        _ => return,
+    let found = yara_match(
+        "
+      rule cryptonight
+      {
+        meta:
+          description = \"Crytonight Miner\"
+        strings:
+          $a = { 00 61 73 6D }
+          $b = { 63 72 79 70 74 6F 6E 69 67 68 74 5F 68 61 73 68 }
+        condition:
+          $a and $b
+      }
+",
+        &body_bytes,
+    );
+    return if found {
+        Ok(Response::from_body(body_bytes))
+    } else {
+        Ok(Response::from_status(StatusCode::NOT_FOUND))
     };
-
-    let id = match auth.get("access_key_id", 8000) {
-        Ok(Some(id)) => id,
-        _ => return,
-    };
-    let key = match auth.get("secret_access_key", 8000) {
-        Ok(Some(key)) => key,
-        _ => return,
-    };
-
-    let client = awsv4::SignatureClient {
-        access_key_id: id,
-        secret_access_token: key,
-    };
-
-    let now = Utc::now();
-    let sig = client.aws_v4_auth(req.get_method().as_str(), req.get_path(), now);
-    req.set_header(header::AUTHORIZATION, sig);
-    req.set_header("x-amz-content-sha256", hash("".to_string()));
-    req.set_header("x-amz-date", now.format("%Y%m%dT%H%M%SZ").to_string());
 }
-
-#[cfg(not(feature = "auth"))]
-// Stub for when authentication feature is disabled
-fn set_authentication_headers(_: &mut Request) {}
 
 /// Removes all headers but those defined in `ALLOWED_HEADERS` from a response.
 fn filter_headers(resp: &mut Response) {
